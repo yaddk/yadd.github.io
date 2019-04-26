621eefc86c57c8be2172a3b77cdde51912693922
Yaddhurshan Krishnakumar <yaddk@YaddhursMacBook.wifi-campus.warwick.ac.uk>
2019-04-26 12:53:03 +0100

    commit 2

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+function menuScroll(item) {
+  if (item == "about-us") {
+    document.getElementById("about-us").scrollIntoView();
+  } else if (item == "instructors") {
+    document.getElementById("instructors").scrollIntoView();
+  } else if (item == "classes") {
+    document.getElementById("classes").scrollIntoView();
+  } else if (item == "contact-us") {
+    document.getElementById("contact-us").scrollIntoView();
+  }
+}
